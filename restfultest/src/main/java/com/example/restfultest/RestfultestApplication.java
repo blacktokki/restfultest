9c58ae753df0be073d553e93dba66b410dd66567
namuwiky <namuwiky@DESKTOP-IF69HEV>
2019-03-29 22:24:02 +0900

    test

---
+++ restfultest/src/main/java/com/example/restfultest/RestfultestApplication.java
@@ -0,0 +1,13 @@
+package com.example.restfultest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfultestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfultestApplication.class, args);
+	}
+
+}
